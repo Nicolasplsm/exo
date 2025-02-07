9e57e5de6d54e9d8b75d70575a5f447971268b5a
Nicolas Plasman <nicolasplasman.pro@gmail.com>
2025-02-07 15:30:53 +0100

    C3: Mise à jour taille fenetre et ajustement du texte

--- Constantes.java
+++ Constantes.java
@@ -1,4 +1,5 @@
 public class Constantes {
     public static final String HELLO_WORLD_TITLE = "Hello World";
     public static final String HELLO_WORLD_TEXT = "Hello World";
+    frame.setSize(300, 300);
 }
