d719ce90056a5b7422c41cda3b44e36dba2b2757
Nicolas Plasman <nicolasplasman.pro@gmail.com>
2025-02-07 15:38:52 +0100

    C6: Couleur de fond en vert

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,13 +6,11 @@ public class HelloWorld {
         JFrame frame = new JFrame(Constantes.HELLO_WORLD_TITLE);
         frame.setSize(200, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().setBackground(Color.YELLOW);
-        label.setForeground(Color.BLUE);
         frame.getContentPane().setBackground(Color.GREEN);
-
 
         // Création du label
         JLabel label = new JLabel(Constantes.HELLO_WORLD_TEXT, SwingConstants.CENTER);
+        label.setForeground(Color.BLUE);
 
         // Ajout du label à la fenêtre
         frame.add(label);
